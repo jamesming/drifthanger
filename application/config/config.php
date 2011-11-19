296d329c27edc6b3b00d130bdc115b3268182535
James Ming <jamesming@gmail.com>
2011-11-19 12:57:51 -0800

    added

--- application/config/config.php
+++ application/config/config.php
@@ -19,7 +19,7 @@
 $urlParts = explode('.', $_SERVER['HTTP_HOST']);
 $subdomain = $urlParts[0];		
 
-echo '<pre>';print_r(  $_SERVER['HTTP_HOST']  );echo '</pre>';  exit;
+
 if( $_SERVER['HTTP_HOST'] == 'jamesming.whsites.net' ){
 	$config['base_url']	= "http://www.jamesming.com/drifthanger/";	
 }elseif( $_SERVER['HTTP_HOST'] == 'localhost' ){
