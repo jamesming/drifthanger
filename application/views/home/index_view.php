d6c329436d0753e0639ae1df0395d95e65efc3c5
James Ming <jamesming@gmail.com>
2011-11-20 14:36:42 -0800

    added

--- application/views/home/index_view.php
+++ application/views/home/index_view.php
@@ -50,7 +50,7 @@ min-height:450px;
 }
 
 .middle .answer-boxes-container {
-    padding-left: 186px;
+    padding-left: 300px;
     margin-top: 24px;
 }
 .middle .answer-boxes-container .answer-box{
